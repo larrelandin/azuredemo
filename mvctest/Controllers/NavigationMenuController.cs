bd52bb15fccd2fcfbf933b1fbb20321ea77a91fa
Larre Ländin <lal@sitecore.net>
2016-03-21 21:27:00 +0100

    First check in. Scaffolding, product rendering, navigation rendering.

---
+++ mvctest/Controllers/NavigationMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvctest.Models;
+using Sitecore.Links;
+
+namespace mvctest.Controllers
+{
+    public class NavigationMenuController : Controller
+    {
+        // GET: NavigationMenu
+        public ActionResult Index()
+        {
+            var home = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
+            var contextItem = Sitecore.Context.Item;
+
+            var menuItems = home.Children
+                .Where(i => i.Template.Key == "product")
+                .Select(i => new NavigationItem
+                {
+                    Title = i["name"],
+                    Url = LinkManager.GetItemUrl(i)
+                })
+                .ToArray();
+
+            return View(menuItems);
+        }
+    }
+}
---
+++ mvctest/Models/NavigationItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Shell.Feeds.FeedTypes;
+
+namespace mvctest.Models
+{
+    public class NavigationItem
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
